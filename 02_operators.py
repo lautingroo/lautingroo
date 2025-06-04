66865222e65db6c4919aec954d1cd1bb859647d4
lautingroo <lautaroavicolli@gmail.com>
2025-06-04 09:22:01 -0300

    Update 02_operators.py
    cambie valores de los print

--- 02_operators.py
+++ 02_operators.py
@@ -3,17 +3,17 @@
 ### Operadores Aritméticos ###
 
 # Operaciones con enteros
-print(3 + 4)
-print(3 - 4)
-print(3 * 4)
-print(3 / 4)
-print(10 % 3)
-print(10 // 3)
-print(2 ** 3)
+print(4 + 4)
+print(4 - 4)
+print(2 * 4)
+print(1 / 4)
+print(10 % 5)
+print(10 // 5)
+print(2 ** 2)
 print(2 ** 3 + 3 - 7 / 1 // 4)
 
 # Operaciones con cadenas de texto
-print("Hola " + "Python " + "¿Qué tal?")
+print("Hola " + "  mARLEY " + "¿como le va?")
 print("Hola " + str(5))
 
 # Operaciones mixtas
@@ -26,16 +26,16 @@ print("Hola " * int(my_float))
 ### Operadores Comparativos ###
 
 # Operaciones con enteros
-print(3 > 4)
-print(3 < 4)
+print(10 > 4)
+print(3 < 6)
 print(3 >= 4)
 print(4 <= 4)
-print(3 == 4)
-print(3 != 4)
+print(3 == 3)
+print(3 != 3)
 
 # Operaciones con cadenas de texto
-print("Hola" > "Python")
-print("Hola" < "Python")
+print("Hola" > "Quintana")
+print("Hola" < "Mundoi")
 print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
 print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
 print("Hola" <= "Python")
