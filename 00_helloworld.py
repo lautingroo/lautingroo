69ea6c60a5be4c55736eac918bb610bb616e58e7
lautingroo <lautaroavicolli@gmail.com>
2025-06-04 10:27:17 -0300

    Update 00_helloworld.py

--- 00_helloworld.py
+++ 00_helloworld.py
@@ -11,19 +11,17 @@ print('Hola geente')
 """
 Este es un
 comentario
-en varias líneas
 """
 
 '''
 Este también es un
-comentario
-en varias líneas
+comentari
 '''
 
 # Cómo consultar el tipo de dato
-print(type("Soy un dato str"))  # Tipo 'str'
-print(type(5))  # Tipo 'int'
+print(type("dato str"))  # Tipo 'str'
+print(type(3))  # Tipo 'int'
 print(type(1.5))  # Tipo 'float'
-print(type(3 + 1j))  # Tipo 'complex'
+print(type(4 + 1j))  # Tipo 'complex'
 print(type(True))  # Tipo 'bool'
-print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
+print(type(print("cadena de texto")))  # Tipo 'NoneType'
